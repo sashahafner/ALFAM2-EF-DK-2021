b7f49cbd848836d9b1fdaf8babd896099b425a43
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-08-29 15:40:14 -0400

    Update ALFAM2mod() call for set 1 to v0.5.0 &
    And fix fit summ subsets

--- model_cal/scripts/extract_preds.R
+++ model_cal/scripts/extract_preds.R
@@ -9,13 +9,14 @@ for(i in 1:length(mods2)) {
 
 # Add predictions with default ALFAM2 model pars (set 1)
 dd <- ALFAM2mod(d2, app.name = 'tan.app', time.name = 'ct', time.incorp = 'time.incorp', 
-                group = 'pmid')
+                group = 'pmid', pars = ALFAM2pars01)
 dd$par.set <- '1'
 dd$er[dd$er > 1] <- 1
 d.pred <- rbind(d.pred, dd)
 
 # ALFAM1 predictions
-da1 <- rbindf(d2, d2cs)
+#da1 <- rbindf(d2, d2cs)
+da1 <- d2
 da1$meas.tech.mm <- TRUE
 dd <- ALFAM1mod(da1, time.name = 'ct', group = 'pmid')
 dd$par.set <- 'ALFAM1'
@@ -23,7 +24,8 @@ dd$er[da1$app.mthd == 'cs'] <- NA
 d.pred <- rbindf(d.pred, dd)
 
 # ALFAM1 predictions but as in EF calcs (trailing hose * reductions)
-da1 <- rbindf(d2, d2cs)
+#da1 <- rbindf(d2, d2cs)
+da1 <- d2
 da1$meas.tech.mm <- TRUE
 da1$app.mthd.bsth <- TRUE
 da1$app.mthd.ts <- FALSE 
@@ -47,8 +49,8 @@ dd$er[da1$app.mthd == 'pi'] <- NA
 d.pred <- rbindf(d.pred, dd)
 
 # Merge with measurements
-#d.pred <- merge(d2, d.pred, by = c('pmid', 'ct', 'dt'), suffixes = c('', '.pred'))
-d.pred <- merge(rbindf(d2, d2cs), d.pred, by = c('pmid', 'ct'), suffixes = c('', '.pred'))
+d.pred <- merge(d2, d.pred, by = c('pmid', 'ct'), suffixes = c('', '.pred'))
+#d.pred <- merge(rbindf(d2, d2cs), d.pred, by = c('pmid', 'ct'), suffixes = c('', '.pred'))
 d.pred$error.er <- d.pred$er.pred - d.pred$er
 
 # Get fixed times
